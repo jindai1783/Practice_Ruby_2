68c64bd75d55e35fa04e7b402b2ff44d0e40bec6
Jin Dai <jindai1783@gmail.com>
2014-12-09 09:50:08 +0000

    Version 4

--- directory.rb
+++ directory.rb
@@ -6,10 +6,21 @@ students = [
 "Jin"
 ]
 
-puts "The students of my cohort at Makers Academy"
-puts "-----------"
-students.each do |student|
-	puts student
+def print_header
+	puts "The students of my cohort at Makers Academy"
+	puts "-----------"
 end
 
-puts "Overall, we have #{student.length} awesome students"
+def print(names)
+	names.each do |name|
+		puts name
+	end
+end
+
+def print_footer(names)
+	puts "Overall, we have #{names.length} awesome students"
+end
+
+print_header
+print(students)
+print_footer(students)
